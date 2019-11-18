22310c34b9cff843be0e9aca9052ad0e51c080be
MinyoungKim <801sanae@gmail.com>
2019-11-19 00:56:40 +0900

    img 취소 및 선택시 동작하는 func 구현

--- 007Delegate-imgPicker02/ViewController.swift
+++ 007Delegate-imgPicker02/ViewController.swift
@@ -24,16 +24,66 @@ class ViewController: UIViewController {
         
         picker.allowsEditing = true // 이미지 편집 기능 on
         
+        picker.delegate = self // delegate 지정
+        
         self.present(picker, animated: false)
     }
     
 }
 
-//MARK: UIImagePickerControllerDelegate
+//MARK: "UIImagePickerControllerDelegate"
 extension ViewController : UIImagePickerControllerDelegate {
     
+    // 이미지 선택하지 않고 취소했을때 호출
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+
+//      * 경합 가능성으로 버그 발생 가능성 증가
+//        self.dismiss(animated: false)
+//        //picker.presentingViewController?.dismiss(animated: false)
+//
+//        let alert = UIAlertController(title: "", message: "이미지 선택이 취소되었습니다.", preferredStyle: .alert)
+//
+//        alert.addAction(UIAlertAction(title: "확인", style: .cancel))
+//
+//        self.present(alert,animated: false)
+
+        
+//      ** 이미지 피커 컨트롤러 창이 완전히 닫힌 후 다음 로직 수행
+//      self.dismiss(animated: false, completion: {() in
+//        self.dismiss(animated: false, completion: {() in
+//            let alert = UIAlertController(title: "", message: "이미지 선택이 취소되었습니다.", preferredStyle: .alert)
+//
+//            alert.addAction(UIAlertAction(title: "확인", style: .cancel))
+//
+//            self.present(alert,animated: false)
+//        })
+//      가독성을 해침.
+        
+
+//      *** 트레일링 클로저 문법?
+        self.dismiss(animated: false){() in
+            let alert = UIAlertController(title: "", message: "이미지 선택이 취소되었습니다.", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "확인", style: .cancel))
+                
+            self.present(alert,animated: false)
+        }
+
+        
+    }
+    
+    // 이미지 선택하면 호출
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        self.dismiss(animated: false){() in
+            // 이미지를 뷰에 표시
+            let img = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+            self.imgView.image = img
+        }
+        
+    }
 }
-//MARK: UINavigationControllerDelegate
+//MARK: "UINavigationControllerDelegate"
 extension ViewController : UINavigationControllerDelegate {
     
 }

ffacdd94463a6afc9a20d76b4a08af3e4d369e30
MinyoungKim <801sanae@gmail.com>
2019-11-19 00:39:38 +0900

    plist 권한 추가 및 이미지 피커 컨트롤러가 라이브러리 나열 및 선택

--- 007Delegate-imgPicker02/ViewController.swift
+++ 007Delegate-imgPicker02/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var imgView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func pick(_ sender: Any) {
+        let picker = UIImagePickerController() // 이미지 피커 컨트롤러 인스턴스 생성
+        
+        picker.sourceType = .photoLibrary // 이미지 소스로 사진 라이브러리 선택
+        
+        picker.allowsEditing = true // 이미지 편집 기능 on
+        
+        self.present(picker, animated: false)
+    }
+    
 }
 
+//MARK: UIImagePickerControllerDelegate
+extension ViewController : UIImagePickerControllerDelegate {
+    
+}
+//MARK: UINavigationControllerDelegate
+extension ViewController : UINavigationControllerDelegate {
+    
+}
